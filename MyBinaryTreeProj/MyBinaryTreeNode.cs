997b04d539c1df61041f24c8d9213d4b7b402c4f
milenapatvakanian-code <milenapatvakanyan29@gmail.com>
2026-03-19 22:20:07 +0400

    գգհյգի

---
+++ MyBinaryTreeProj/MyBinaryTreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace MyBinaryTreeProj
+{
+    public class MyBinaryTreeNode<TNode> : IComparable<TNode>
+        where TNode : IComparable<TNode>
+    {
+        public TNode Value { get; private set; }
+        public MyBinaryTreeNode<TNode> Left { get; set; }
+        public MyBinaryTreeNode<TNode> Right { get; set; }
+
+        public MyBinaryTreeNode(TNode value)
+        {            
+            Value = value;  
+        }
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+    }
+}
